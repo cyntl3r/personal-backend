85a0d07a6168af2d561f9821e06233be8912e5af
cyntler <damian@cyntler.com>
2021-04-05 23:03:26 +0200

    Remove prisma files

--- prisma/migrations/20210218203427_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-    "title" TEXT NOT NULL,
-    "content" TEXT,
-
-    PRIMARY KEY ("id")
-);
